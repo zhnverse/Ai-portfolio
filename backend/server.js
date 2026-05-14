22f92764d5528b55a7e18d3a4774d270bf12afe2
zhnverse <your@email.com>
2026-05-15 01:26:02 +0600

    Fix catch-all route using app.use to support Express 5

--- backend/server.js
+++ backend/server.js
@@ -91,7 +91,7 @@ app.post('/api/chat', async (req, res) => {
 
 // Serve static React frontend in production
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
-app.get('/(.*)', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
